fad83da3e810b665bc3da18e157fa9789946c20e
chinmaym07 <chinmay.mehta07@gmail.com>
2020-04-12 02:46:30 +0530

    Updates

---
+++ precache-manifest.c293eb3c20a44480c12b988fa3dfe05e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9110f1e4e828244022d6af5cd577132",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "0f85acd9eee2e22a6911",
+    "url": "/Monster-Rolodex/static/css/main.6b1a3274.chunk.css"
+  },
+  {
+    "revision": "9d84f5b306774dfc9f1a",
+    "url": "/Monster-Rolodex/static/js/2.91e906d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.91e906d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f85acd9eee2e22a6911",
+    "url": "/Monster-Rolodex/static/js/main.646e0675.chunk.js"
+  },
+  {
+    "revision": "7dcc0088d9e44091de62",
+    "url": "/Monster-Rolodex/static/js/runtime-main.ce606da0.js"
+  }
+]);
